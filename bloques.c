595fa8ed58f84b81c6ed3083adb09059ffc3a906
Otto <ottovasdel@gmail.com>
2024-02-21 18:56:17 +0100

    init

---
+++ bloques.c
@@ -0,0 +1,78 @@
+/**
+ * @author Otto Vásquez
+ * @author Bernat Parera
+ *
+*/
+
+#include "bloques.h"
+
+static int descriptor = 0;
+
+
+/**
+ * Monta el dispositivo virtual
+*/
+int bmount(const char* camino) {
+    descriptor = open(camino, O_RDWR | O_CREAT, 0666);
+    if (descriptor == FALLO){
+        perror("Error al abrir el archivo");
+        return FALLO;
+    } else {
+        return descriptor;
+    }
+}
+
+
+/**
+ * Desmonta el dispositivo virtual
+*/
+int bumount() {
+    int exitFile = close(descriptor);
+    if (exitFile == FALLO){
+        perror("Error al cerrar el fichero");
+        return FALLO;
+    } else {
+        return EXITO;
+    }
+}
+
+
+/**
+ * Escribe 1 bloque del dispositivo virtual
+*/
+int bwrite(unsigned int nbloque, const void *buf){
+    off_t desplazamiento = nbloque * BLOCKSIZE;
+    if(lseek(descriptor, desplazamiento, SEEK_SET) == FALLO){
+        perror("Error al realizar seek para escritura");
+        return FALLO;
+    }
+
+    ssize_t bytesEscritos =  write(descriptor, buf, BLOCKSIZE);
+    if (bytesEscritos == FALLO){
+        perror("Error en escritura");
+        return FALLO;
+    } else {
+        return bytesEscritos;
+    }
+}
+
+
+/**
+ * Lee 1 bloque del dispositivo virtual
+*/
+int bread(unsigned int nbloque, void *buf){
+    off_t desplazamiento = nbloque * BLOCKSIZE;
+    if(lseek(descriptor, desplazamiento, SEEK_SET) == FALLO){
+        perror("Error al realizar seek para lectura");
+        return FALLO;
+    }
+
+    ssize_t bytesLeidos =  read(descriptor, buf, BLOCKSIZE);
+    if (bytesLeidos == FALLO){
+        perror("Error en lectura");
+        return FALLO;
+    } else {
+        return bytesLeidos;
+    }
+
+}
---
+++ mi_mkfs.c
@@ -0,0 +1,27 @@
+#include "bloques.h"
+// INCLUDE BLOQUES.H
+
+int main(int argc, char **argv)
+{
+    ////Comprobamos sintaxis
+    if (argc < 3)
+    {
+        fprintf(stderr, "Error al introducir el comando\n");
+        return -1;
+    }
+    // Montamos dispositivo usando el nombre introducido en consola
+    bmount(argv[1]);
+    // Creamos un bloque libre (todo 0)
+    unsigned char libre[BLOCKSIZE]; // esto esta en bloques.h
+    // Ponemos los bits de cada byte del bloque a 0.
+    memset(libre, 0, BLOCKSIZE);
+    // Convertimos la cantidad de nbloques pasada por consola a un entero.
+    int nbloques = atoi(argv[2]);
+    // bucle para escribir nbloques veces un bloque libre
+    for (int i = 0; i < nbloques; i++)
+    {
+        bwrite(i, libre);
+    }
+    bumount(argv[1]);
+    return 0;
+}
