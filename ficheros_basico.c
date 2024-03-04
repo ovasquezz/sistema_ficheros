b6ba34ff49c2127107ebf06f2f23512d13af0a26
Otto <ottovasdel@gmail.com>
2024-03-04 15:05:45 +0100

    nivel 2

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -45,44 +45,60 @@ int initSB(unsigned int nbloques, unsigned int ninodos) {
 
 int initMB() {
     struct superbloque SB;
-
-    if (bread(posSB, &SB) < 0) {
-        perror("Error lectura Super Bloque\n");
-        return FALLO;
-    }
-
-    // TODO
-
-
-
-
-}
-
-int initAI() {
-
-    struct superbloque SB;
-    if (bread(posSB, &SB) < 0) {
-        perror("Error lectura Super Bloque\n");
-        return FALLO;
-    }
-
-    int contInodos = SB.posPrimerInodoLibre + 1;
-    struct inodo inodos[BLOCKSIZE / INODOSIZE];
-    for (int i = SB.posPrimerBloqueAI; i <= SB.posUltimoBloqueAI; i++) {
-        int over = 0;
-        for (int j = 0; (j < (BLOCKSIZE / INODOSIZE)) && (!over); j++) {
-            inodos[j].tipo = 'l';
-            if (contInodos < SB.totInodos) {
-                inodos[j].punterosDirectos[0] = contInodos;
-                contInodos++;
-            } else {
-                inodos[j].punterosDirectos[0] = UINT_MAX;
-                over = 1;
-            }
+    bread(posSB, &SB);
+    int used_blocks = SB.posPrimerBloqueMB;
+    int metadata = tamMB(SB.totBloques) + tamAI(SB.totInodos) + 1;
+    SB.cantBloquesLibres = SB.cantBloquesLibres - metadata;
+    if ((metadata / 8 / BLOCKSIZE) > 1) {
+        unsigned char* libre = malloc(BLOCKSIZE);
+        for (; metadata > BLOCKSIZE; used_blocks++) {
+            memset(libre, 255, BLOCKSIZE);
+            bwrite(1 + used_blocks, libre);
+            // Miramos si hace falta un bloque completo más para guardar los metadata.
+            metadata = metadata / BLOCKSIZE;
         }
-        bwrite(i, inodos);
+    }
+    int bufferSize = metadata / 8;
+    unsigned char bufferMB[BLOCKSIZE];
+    memset(bufferMB, 0, BLOCKSIZE);
+    for (int i = 0; i < bufferSize; i++) {
+        bufferMB[i] = 255;
+    }
+    int datos_restantes = metadata % 8;
+    unsigned char resto = 0;
+    for (int i = 0; i < datos_restantes; i++) {
+        resto += 1 << (7 - i);
+    }
+    bufferMB[bufferSize] = resto;
+    bwrite(used_blocks, bufferMB);
+    if (bwrite(posSB, &SB) < 0) {
+        fprintf(stderr, "Error en la escritura del MB\n");
+        return FALLO;
     }
     return EXITO;
 }
 
-
+int initAI() {
+    struct superbloque SB;
+    bread(posSB, &SB);
+    int bufferSize = BLOCKSIZE / INODOSIZE;
+    struct inodo inodos[BLOCKSIZE / INODOSIZE];
+    int inodoCount = SB.posPrimerInodoLibre + 1;
+    for (int i = SB.posPrimerBloqueAI; i <= SB.posUltimoBloqueAI; i++) {
+        bread(i, inodos);
+        for (int j = 0; j < bufferSize; j++) {
+            inodos[j].tipo = 'l';
+            if (inodoCount < SB.totInodos) {
+                inodos[j].punterosDirectos[0] = inodoCount;
+                inodoCount++;
+            } else {
+                inodos[j].punterosDirectos[0] = UINT_MAX;
+                j = bufferSize;
+            }
+        }
+        if (bwrite(i, inodos) < 0) {
+            fprintf(stderr, "Error al iniciar el AI\n");
+        };
+    }
+    return bwrite(posSB, &SB);
+}
